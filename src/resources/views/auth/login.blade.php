f7119b9bb4bdf1cac9993e9918ac44840ca47951
hogasuke <touhoufuhai.hogasuke@gmail.com>
2026-03-10 14:58:39 +0900

    ログイン画面レイアウト適用

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 @section('content')
 <div class="login-form__content">
   <div class="login-form__heading">
-    <h2>ログイン</h2>
+    <h2>Login</h2>
   </div>
   <form class="form" action="/login" method="post" novalidate>
     @csrf
